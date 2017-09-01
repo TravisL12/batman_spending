ca28b07e4e4d7115f3cf87157ee8ee3d45c66d4c
Travis Lawrence <tlawrence@tumblr.com>
2017-09-01 13:25:17 -0700

    Group by year, month, day

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,8 +1,58 @@
 class TransactionsController < ApplicationController
 
     def index
-        @transaction = Transaction.joins(:category)
-        render json: @transaction
+        @trans = Transaction.joins(:category)
+
+        # {
+        #     2011 => {
+        #         total: 1234,
+        #         month => {
+        #             total: 123,
+        #             0 => {
+        #                 total: 12,
+        #                 day => []
+        #             },
+        #             1 => {
+        #                 total: 34,
+        #                 day => []
+        #             }
+        #         }
+        #     },
+        #     2012 => { ... },
+        #     2013 => { ... },
+        #     2014 => { ... },
+        # }
+
+        @data = {}
+        @years_grouped = @trans.group_by{|tran| tran.date.year}
+
+        @years_grouped.each do |year, trans|
+            @data[year] = {
+                'month' => {},
+                'total' => trans.map(&:amount).inject(0,&:+) / 100
+            }
+
+            months_grouped = trans.group_by{ |tran| tran.date.month }
+            months_grouped.each do |month, trans2|
+                @data[year]['month'][month] = {
+                    'day' => [],
+                    'total' => trans2.map(&:amount).inject(0,&:+) / 100
+                }
+
+                days_grouped = trans2.group_by{ |tran| tran.date.day }
+                days_in_month = Date.new(year, month, -1)
+
+                1.upto(days_in_month.day) do |day|
+                    @data[year]['month'][month]['day'].push({
+                        'day' => day,
+                        'total' => 0,
+                        'transactions' => days_grouped[day] || []
+                    })
+                end
+            end
+        end
+
+        render json: @data
     end
 
 end
