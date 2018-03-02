c9f315ed77641f2089ee84ad083170ddf99aee8c
Travis Lawrence <tlawrence@tumblr.com>
2018-03-02 09:38:29 -0800

    Auth is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  post 'user_token' => 'user_token#create'
-  get 'users/new'
 
   namespace :v1 do
     resources :transactions, only: [:create, :destroy]
+    resources :users
     get '/transactions(/:year)', to: 'transactions#index'
+    post 'user_token' => 'user_token#create'
   end
 
 end
